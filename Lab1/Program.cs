3cc4386405b8dd03534ad7493c59656b28d2423c
Маштак Игорь <io.mashtak@yandex.ru>
2025-09-07 00:39:54 +0300

    Решён уровень Blue

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,18 +1,95 @@
-﻿namespace Lab1
+﻿using System.ComponentModel;
+using System.Numerics;
+
+namespace Lab1
 {
     public class Program
     {
         public static void Main()
         {
+            /*
             White white = new White();
+            Console.WriteLine("#1");
             Console.WriteLine(white.Task1(1.2));
             Console.WriteLine(white.Task1(-3.2));
+            Console.WriteLine("#2");
             Console.WriteLine(white.Task2(2));
+            Console.WriteLine("#3");
             Console.WriteLine(white.Task3(2, 1));
-            //Console.WriteLine(white.Task4(-2.5, 1.89));
-            //Console.WriteLine(white.Task5(0.5));
-            //Console.WriteLine(white.Task6(2.5, 1.89, 3));
+            Console.WriteLine(white.Task4(-2.5, 1.89));
+            Console.WriteLine(white.Task5(0.5));
+            Console.WriteLine(white.Task6(2.5, 1.89, 3));
+            Console.WriteLine("#7");
             Console.WriteLine(white.Task7(2));
+            */
+
+            Blue blue = new Blue();
+            Console.WriteLine("Blue");
+            Console.WriteLine("#1");
+            Console.WriteLine(blue.Task1(3, 0));
+            Console.WriteLine(blue.Task1(0, -2));
+            Console.WriteLine(blue.Task1(0, 0));
+            Console.WriteLine(blue.Task1(3, 6));
+            Console.WriteLine(blue.Task1(-64, -120));
+            Console.WriteLine(blue.Task1(-2, 6));
+            Console.WriteLine(blue.Task1(2, -3));
+            Console.WriteLine("#2");
+            Console.WriteLine(blue.Task2(4));
+            Console.WriteLine(blue.Task2(-2));
+            Console.WriteLine(blue.Task2(1.00000005));
+            Console.WriteLine(blue.Task2(-0.219));
+            Console.WriteLine(blue.Task2(10201.0001));
+            Console.WriteLine("#3");
+            Console.WriteLine(blue.Task3(19, 721));
+            Console.WriteLine(blue.Task3(39, 4));
+            Console.WriteLine(blue.Task3(29, 0));
+            Console.WriteLine(blue.Task3(0, 0));
+            Console.WriteLine(blue.Task3(72, 12));
+            Console.WriteLine(blue.Task3(72, -6));
+            Console.WriteLine(blue.Task3(3, 1));
+            Console.WriteLine(blue.Task3(0, 12));
+            Console.WriteLine("#4");
+            Console.WriteLine(blue.Task4(4.1, -233, -32.124));
+            Console.WriteLine(blue.Task4(1, -23, 8));
+            Console.WriteLine(blue.Task4(39, -16, 24));
+            Console.WriteLine(blue.Task4(29.0, -23, 91));
+            Console.WriteLine(blue.Task4(1, 4.0, -3));
+            Console.WriteLine("#5");
+            Console.WriteLine(blue.Task5(-4));
+            Console.WriteLine(blue.Task5(-1.000001));
+            Console.WriteLine(blue.Task5(-1));
+            Console.WriteLine(blue.Task5(-0.999999));
+            Console.WriteLine(blue.Task5(-0.7));
+            Console.WriteLine(blue.Task5(-0.4));
+            Console.WriteLine(blue.Task5(-0.00001));
+            Console.WriteLine(blue.Task5(0));
+            Console.WriteLine(blue.Task5(0.00001));
+            Console.WriteLine(blue.Task5(20239));
+            Console.WriteLine("#6");
+            Console.WriteLine(blue.Task6(5 * 5 * Math.PI + 0.1, 10 * 10));
+            Console.WriteLine(blue.Task6(5 * 5 * Math.PI, 10 * 10));
+            Console.WriteLine(blue.Task6(5 * 5 * Math.PI - 0.1, 10 * 10));
+            Console.WriteLine("#7");
+            Console.WriteLine(blue.Task7(0.1, 0.2));
+            Console.WriteLine(blue.Task7(0.1, -0.2));
+            Console.WriteLine(blue.Task7(-0.1, 0.2));
+            Console.WriteLine(blue.Task7(0, -0.2));
+            Console.WriteLine(blue.Task7(0.2, 0.1));
+            Console.WriteLine(blue.Task7(0.2, -0.1));
+            Console.WriteLine(blue.Task7(-0.2, 0.1));
+            Console.WriteLine(blue.Task7(-0.2, 0));
+            Console.WriteLine(blue.Task7(5, 5));
+            Console.WriteLine(blue.Task7(-5, 5));
+            Console.WriteLine(blue.Task7(5, -5));
+            Console.WriteLine(blue.Task7(-5, -5));
+            Console.WriteLine(blue.Task7(0, 0));
+            Console.WriteLine("#8");
+            Console.WriteLine(blue.Task8(4, 6, 9));
+            Console.WriteLine(blue.Task8(0, 0, 4));
+            Console.WriteLine(blue.Task8(2, 3, 4));
+            Console.WriteLine(blue.Task8(0, 1, 1));
+            Console.WriteLine(blue.Task8(0, 0, 1));
+            Console.WriteLine(blue.Task8(0, 0, 0));
         }
     }
 }
